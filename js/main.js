0d38ce9305d63d0904aeb9b99a016d6d8e55e8c2
aruanfreedom <aruanfreedom@outlook.com>
2016-05-23 01:34:02 +0600

    version 1 angular)

---
+++ js/main.js
@@ -0,0 +1,35 @@
+;
+(function() {
+    var grow = angular.module('growApp', []);
+
+    grow.controller("usersCtrl", function($scope, $http) {
+        var url = '../json/users.json';
+
+        $http.get(url).success(function(response) {
+            $scope.users = response;
+        });
+
+        $scope.predicate = 'first_name';
+        $scope.reverse = true;
+        $scope.order = function(predicate) {
+            $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+            $scope.predicate = predicate;
+        };
+
+        $http.get('../json/combobox.json').success(function(response) {
+            $scope.comboboxes = response;
+        });
+
+        $scope.select = function() {
+            var url = $scope.selectBtn.url;
+            $http.get(url).success(function(response) {
+                $scope.users = response;
+            });
+        };
+
+        $scope.date = {
+            value: new Date(2016, 5, 22)
+        };
+    });
+
+})();
